5f864d91d9749b6d3b969de2b5cfdde543e75afb
stephen ligtenberg <bountyhunter24@gmail.com>
2023-04-01 22:27:41 -0600

    commit tree 9f458055b420acfbfba81881ebc00d2fbeea6709

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # byebug
+    students = if params[:name]
+      # byebug
+      Student.where('lower(first_name) = lower(?)', params[:name]).or(Student.where('lower(last_name) = lower(?)', params[:name]))
+    else
+      Student.all
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
